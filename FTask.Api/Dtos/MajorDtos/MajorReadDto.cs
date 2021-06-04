e12928a08f232ba91e62af8eee2b772d84716a6c
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2021-06-04 13:46:07 +0700

    Remove unuse import...

--- FTask.Api/Dtos/MajorDtos/MajorReadDto.cs
+++ FTask.Api/Dtos/MajorDtos/MajorReadDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FTask.Api.Dtos.MajorDtos
+﻿namespace FTask.Api.Dtos.MajorDtos
 {
     public class MajorReadDto
     {
