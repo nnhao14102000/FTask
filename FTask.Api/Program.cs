23f836373dd17a7bea18a6a3d5a993b9d90d9912
nnhao14102000 <nnhao9a3@gmail.com>
2021-06-09 13:48:48 +0700

    Add DB auto create if DB is not exited...

--- FTask.Api/Program.cs
+++ FTask.Api/Program.cs
@@ -1,5 +1,10 @@
+using FTask.Data.Helpers;
+using FTask.Data.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace FTask.Api
 {
@@ -7,7 +12,35 @@ namespace FTask.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var env = services.GetRequiredService<IWebHostEnvironment>();
+                    var context = services.GetRequiredService<FTaskContext>();
+                    if (env.IsStaging() || env.IsDevelopment())
+                    {
+                        DBInitializer.Initialize(context);
+                    }
+                    if (env.IsProduction())
+                    {
+                        //DBInitializer.Initialize(context);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured creating the CloudDB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ FTask.Data/Helpers/DBInitializer.cs
@@ -0,0 +1,15 @@
+﻿using FTask.Data.Models;
+
+namespace FTask.Data.Helpers
+{
+    public static class DBInitializer
+    {
+        public static void Initialize(FTaskContext context)
+        {
+            if (context.Database.EnsureCreated() == false)
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
