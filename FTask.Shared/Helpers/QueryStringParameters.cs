2527bc27583ed2592d1ac916920b9f1ce131bd64
nnhao14102000 <nnhao9a3@gmail.com>
2021-07-19 11:18:23 +0700

    Modify default page size

--- FTask.Shared/Helpers/QueryStringParameters.cs
+++ FTask.Shared/Helpers/QueryStringParameters.cs
@@ -5,7 +5,7 @@
         const int maxPageSize = 500;
         public int PageNumber { get; set; } = 1;
 
-        private int _pageSize = 200;
+        private int _pageSize = 10;
         public int PageSize
         {
             get
