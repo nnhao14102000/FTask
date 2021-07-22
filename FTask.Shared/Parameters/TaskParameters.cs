37ed7ab736f4e8a7dc2df7d7fc000be1c03a9fcd
nnhao14102000 <nnhao9a3@gmail.com>
2021-07-22 18:50:58 +0700

    Modify sort

--- FTask.Shared/Parameters/TaskParameters.cs
+++ FTask.Shared/Parameters/TaskParameters.cs
@@ -4,12 +4,9 @@ namespace FTask.Shared.Parameters
 {
     public class TaskParameters : QueryStringParameters
     {
-        public enum Types {
-            LastestTime, HighestPriority
-        }
         public string TaskSearchValue { get; set; }
         public int PlanTopicId { get; set; }
-        public Types? SortOptions { get; set; }
+        public bool SortWithHighPriority { get; set; }
         public bool? IsComplete { get; set; }
     }
 }
